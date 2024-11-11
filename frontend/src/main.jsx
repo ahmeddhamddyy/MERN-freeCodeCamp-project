2136756e65d1b79a872c78c558c73ef21a6a84de
Ahmed Hamdy <medo3_200162@yahoo.com>
2024-11-11 13:26:42 +0200

    Created Successful notification , resolved most of the syntaxErrors , and downloaded new packages like chakra , toast , and zustand

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';  // Correct import
 import App from './App.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ChakraProvider>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
 );
